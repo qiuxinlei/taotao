17b5ed4c693b3c2650db4dc5e5766d2de4d2a094
qiuxinlei <1242829404@qq.com>
2017-12-26 21:53:14 +0800

    更新2017年12月26日17:17:26 添加了多媒体文件解析器

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PictureController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PictureController.java
@@ -2,6 +2,7 @@ package com.taotao.controller;
 
 import com.taotao.pojo.PictureResult;
 import com.taotao.service.PictureService;
+import com.taotao.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PictureController {
@@ -19,11 +19,11 @@ public class PictureController {
     //上传文件
     @RequestMapping(value = "/pic/upload")
     @ResponseBody
-    public PictureResult uploadFile(@RequestParam(value = "uploadFile",required = false,defaultValue = "") MultipartFile uploadFile){
+    public String uploadFile(@RequestParam(value = "uploadFile",required = false,defaultValue = "") MultipartFile uploadFile){
         PictureResult result = pictureService.uploadPic(uploadFile);
-
         System.out.println(result);
-        return result;
+        String json = JsonUtils.objectToJson(result);
+        return json;
     }
 
 }
